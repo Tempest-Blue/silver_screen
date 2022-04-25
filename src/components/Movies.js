3203143812ffe1d36dbfb5076f7ef22d96c939dc
Tien Vo <Tempest-Blue@users.noreply.github.com>
2022-04-25 04:27:56 -0700

    chore: add links

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -8,7 +8,8 @@ import { withStyles } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import InfiniteScroll from "react-infinite-scroll-component"
 import Fade from 'react-reveal/Fade'
-import SimpleModal from './modal.js'
+import youtube from '../images/youtube.png'
+import rotten_tomatoes from '../images/rotten_tomatoes.svg'
 
 const useStyles = theme => ({
   main: {
@@ -31,22 +32,49 @@ const useStyles = theme => ({
     },
   },
   card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    border: 'solid #383D3F 1px',
+    'height': '100%',
+    'display': 'flex',
+    'flexDirection': 'column',
+    'border': 'solid #383D3F 1px',
     'border-radius': '10px',
     'background-color': '#181A1B',
     '&:hover': {
-      'cursor': 'pointer',
       'border': '5px white solid',
     }
   },
   cardMedia: {
-    paddingTop: '56.25%', // 16:9
-    height: '100%',
-    // 'background-size': 'contain',
+    'paddingTop': '56.25%', // 16:9
+    'height': '100%',
     'background-color': '#1C1E1F',
+  },
+  hovering: {
+    'height': '100%',
+    'width': '100%',
+    'display': 'grid',
+    'place-items': 'center',
+    'grid-template': '\'inner-div\'',
+  },
+  hoverImg: {
+    'height': '100%',
+    'width': '100%',
+    'opacity': '20%',
+    'background-size': 'cover',
+    'grid-area': 'inner-div'
+  },
+  hoverLinks: {
+    'grid-area': 'inner-div',
+    'display': 'flex',
+    'justify-content': 'center',
+    'align-items': 'center',
+    'flex-direction': 'column',
+
+    '& img': {
+      'height': 'auto',
+      'width': '50%',
+      'cursor': 'pointer',
+      'z-index': '100',
+      'margin': '10px',
+    }
   },
   cardContent: {
     // flexGrow: 1,
@@ -59,7 +87,7 @@ const useStyles = theme => ({
     'max-height': '75%',
   },
   label: {
-    height: '10%',
+    'height': '10%',
     'font-family': 'Open Sans Regular,Helvetica Neue,Helvetica,Arial,sans-serif',
   },
   infiniteScroll: {
@@ -73,12 +101,36 @@ class Movies extends React.Component {
     super(props)
     this.state = {
       page: 1,
-      items: []
+      items: [],
+      opened: false,
     }
   }
 
   componentDidMount() {
     this.getMovies()
+    function start() {
+      // 2. Initialize the JavaScript client library.
+      window.gapi.client.init({
+        'apiKey': process.env.REACT_APP_YT_API_KEY,
+        // Your API key will be automatically added to the Discovery Document URLs.
+        'discoveryDocs': ['https://people.googleapis.com/$discovery/rest'],
+        // clientId and scope are optional if auth is not required.
+        'clientId': 'YOUR_WEB_CLIENT_ID.apps.googleusercontent.com',
+        'scope': 'profile',
+      }).then(function() {
+        // 3. Initialize and make the API request.
+        return gapi.client.request({
+          'path': 'https://people.googleapis.com/v1/people/me?requestMask.includeField=person.names',
+        })
+      }).then(function(response) {
+        console.log(response.result);
+      }, function(reason) {
+        console.log('Error: ' + reason.result.error.message);
+      })
+    };
+    // 1. Load the JavaScript client library.
+    window.gapi.load('client', start);
+    window.gapi.sea
   }
 
   getMovies = () => {
@@ -107,16 +159,22 @@ class Movies extends React.Component {
     items.map((movie) => {
       let date = new Date(movie.release_date)
       movie.release_date = date.getFullYear()
+      return []
     })
 
     let newState = {
       items: this.state.items.concat(items),
     }
-    if (this.state.page == 1) {
+    if (this.state.page === 1) {
       newState.isLoaded = true
     }
     newState.page = this.state.page + 1
     this.setState(newState)
+  }
+
+  openInNewTab = (url) => {
+    const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+    if (newWindow) newWindow.opener = null
   }
 
   render() {
@@ -143,12 +201,30 @@ class Movies extends React.Component {
                   <Grid className={classes.movieGrid} spacing={2} container>
                     {items.map((movie) => (
                       <Grid item key={movie.id} xs={12} sm={5} md={3} className={classes.gridItem}>
-                        <Card className={classes.card}>
-                          <CardMedia
-                            className={classes.cardMedia}
-                            image={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-                            title={movie.title}
-                          />
+                        <Card
+                          className={classes.card}
+                          onMouseEnter={() => this.setState({ [`${movie.id}_opened`]: !this.state[`${movie.id}_opened`] })}
+                          onMouseLeave={() => this.setState({ [`${movie.id}_opened`]: !this.state[`${movie.id}_opened`] })}>
+                          {
+                            !this.state[`${movie.id}_opened`] &&
+                            <CardMedia
+                              className={classes.cardMedia}
+                              image={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                              title={movie.title}
+                            />
+                          }
+                          {
+                            this.state[`${movie.id}_opened`] &&
+                            <>
+                              <div className={classes.hovering}>
+                                <div className={classes.hoverImg} style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.poster_path})` }}></div>
+                                <div className={classes.hoverLinks}>
+                                  <img src={youtube} onClick={() => this.openInNewTab(`https://www.youtube.com/results?search_query=${movie.title} ${movie.release_date}`)} />
+                                  <img src={rotten_tomatoes} onClick={() => this.openInNewTab(`https://www.rottentomatoes.com/m/${movie.title.replaceAll(' ', '_').replace(/[^a-zA-Z0-9_]/g, '')}`)} />
+                                </div>
+                              </div>
+                            </>
+                          }
                           <CardContent className={classes.cardContent}>
                             <Typography gutterBottom variant="subtitle1" component="h2" className={classes.movieTitle}>
                               {movie.title}
@@ -165,7 +241,7 @@ class Movies extends React.Component {
               </InfiniteScroll>
             </Grid>
           </Grid>
-        </main>
+        </main >
       )
     }
   }
